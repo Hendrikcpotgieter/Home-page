c5b8812a3af8fcba4bbf8278d1ba7e82fb261a0f
Hendrik Christoffel Potgieter <44273195+Hendrikcpotgieter@users.noreply.github.com>
2024-11-04 14:03:05 +0200

    V=1
    This site needs to have join, member-to-member communication, an idea area, a discussion area, Google Earth integrations or other/better, picture upload, voice message messenger, collaborative drawings team access, and whatever else a rescue space travellers and earth protection services platform needs.

---
+++ mensdomoorlewing/js/menu.js
@@ -0,0 +1,6 @@
+function navBarFunction() {
+    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+}
+
+
+// https://foolishdeveloper.com/navigation-bar-html-css-javascript/  48
